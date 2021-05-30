ee90d4c34ca2060574ca4af562d3c4e86a82e0d3
Maximo Sandoval <80641742+maximosandoval@users.noreply.github.com>
2021-05-30 13:18:27 -0600

    Updated JS and CSS
    1. Updated CSS with removal of elements
    2. Updated API in JS

--- script/script.js
+++ script/script.js
@@ -105,93 +105,59 @@ function displayWeatherBoard(city) {
   uvIndex.textContent = "UV Index:" + " " + data.current.uvi;
 
   //Target UV Color changes
-  indexEl.text("UV Index: ");
-  var indexNumber = parseFloat(responseTwo.value);
-  var indexNumberEl = $("<p>");
-  indexNumberEl.text(indexNumber);
-  indexNumberEl.attr("id", "index-number");
+  // indexEl.text("UV Index: ");
+  // var indexNumber = parseFloat(responseTwo.value);
+  // var indexNumberEl = $("<p>");
+  // indexNumberEl.text(indexNumber);
+  // indexNumberEl.attr("id", "index-number");
 
-  if (uvIndex <= 2) {
-    indexNumberEl.addClass("d-inline p-2 bg-success text-white");
-  } else if (uvIndex >= 3 && indexNumber <= 7) {
-    indexNumberEl.addClass("d-inline p-2 bg-warning text-white");
-  } else {
-    indexNumberEl.addClass("d-inline p-2 bg-danger text-white");
+  // if (uvIndex <= 2) {
+  //   indexNumberEl.addClass("uv-data-index text-white");
+  // } else if (uvIndex >= 3 && indexNumber <= 7) {
+  //   indexNumberEl.addClass("uv-data-index text-white");
+  // } else {
+  //   indexNumberEl.addClass("uv-data-index text-white");
+  // }//
+  //}//
+
+  // creating function to display forecast cards
+  function displayFutureForecasts(city) {
+    // pulling data out of local storage
+    let data = JSON.parse(localStorage.getItem(city));
+    // grabbing html element for text content
+    let forecast = document.querySelectorAll(".card-forecast");
+    forecastContainerEl.classList.remove("hide");
+    // creating loop to empty out div once new city is submitted for search
+    for (let i = 0; i < forecast.length; i++) {
+      forecast[i].innerHTML = "";
+    }
+
+    // looping through data to get daily forecast
+    // incrementing daily for 5 day forecast
+    for (let i = 0; i < forecast.length; i++) {
+      // creating html element
+      const date = document.createElement("h5");
+      const icon = document.createElement("img");
+      const temp = document.createElement("p");
+      const wind = document.createElement("p");
+      const humidity = document.createElement("p");
+
+      // assigning value to text content
+      date.textContent = moment()
+        .add(i + 1, "days")
+        .format("dddd, MMMM Do");
+      icon.setAttribute(
+        "src",
+        "http://openweathermap.org/img/wn/" +
+          data.daily[i].weather[0].icon +
+          "@2x.png"
+      );
+      temp.textContent = "Temp:" + " " + data.daily[i].temp.day + "°F";
+      wind.textContent = "Wind:" + " " + data.daily[i].wind_speed + " " + "MPH";
+      humidity.textContent = "Humidity:" + " " + data.daily[i].humidity + "%";
+
+      // appending created elements
+      forecast[i].append(date, icon, temp, wind, humidity);
+    }
   }
 }
-
-// creating function to display forecast cards
-function displayFutureForecasts(city) {
-  // pulling data out of local storage
-  let data = JSON.parse(localStorage.getItem(city));
-  // grabbing html element for text content
-  let forecast = document.querySelectorAll(".card-forecast");
-  forecastContainerEl.classList.remove("hide");
-  // creating loop to empty out div once new city is submitted for search
-  for (let i = 0; i < forecast.length; i++) {
-    forecast[i].innerHTML = "";
-  }
-
-  // looping through data to get daily forecast
-  // incrementing daily for 5 day forecast
-  for (let i = 0; i < forecast.length; i++) {
-    // creating html element
-    const date = document.createElement("h5");
-    const icon = document.createElement("img");
-    const temp = document.createElement("p");
-    const wind = document.createElement("p");
-    const humidity = document.createElement("p");
-
-    // assigning value to text content
-    date.textContent = moment()
-      .add(i + 1, "days")
-      .format("dddd, MMMM Do");
-    icon.setAttribute(
-      "src",
-      "http://openweathermap.org/img/wn/" +
-        data.daily[i].weather[0].icon +
-        "@2x.png"
-    );
-    temp.textContent = "Temp:" + " " + data.daily[i].temp.day + "°F";
-    wind.textContent = "Wind:" + " " + data.daily[i].wind_speed + " " + "MPH";
-    humidity.textContent = "Humidity:" + " " + data.daily[i].humidity + "%";
-
-    // appending created elements
-    forecast[i].append(date, icon, temp, wind, humidity);
-  }
-}
-
-////////////////////////////////////////
-///// OLD CODE ////////////////////////
-
-// const inputEl = document.getElementById("city-search");
-// const searchEl = document.getElementById("search-button");
-// const nameEl = document.getElementById("city-name");
-// const currentTempEl = document.getElementById("temperature");
-// const currentHumidityEl = document.getElementById("humidity");
-// const currentWindEl = document.getElementById("wind-speed");
-// const currentUVEl = document.getElementById("UV-index");
-// const historyEl = document.getElementById("history");
-
-// function initPage() {
-//Local storage to place citites into local memory storage
-// let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-// console.log(searchHistory);
-
-//API call to OpenWeather for lat and long
-//   const APIKey =
-//     "https://maps.openweathermap.org/maps/2.0/weather/{op}/{z}/{x}/{y}&appid={f2204c94618c50789292117a9e820701}";
-// }
-
-//Event listner for search button
-// searchEl.addEventListener("click", function () {
-//   const searchTerm = inputEl.value;
-//   getWeather(searchTerm);
-//   searchHistory.push(searchTerm);
-//   localStorage.setItem("search", JSON.stringify(searchHistory));
-//   renderSearchHistory();
-// });
-// function getWeather(cityName) {
-//   let searchQueryURl =
-//     "https://api.openweathermap.org/data/2.5/weather?id={city id}&appid={f2204c94618c50789292117a9e820701}";
-// }
